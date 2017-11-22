f593a9170a8f075182cdb020303cc538888ec776
Paul Vorbach <paul@vorba.ch>
2017-11-22 22:30:40 +0100

    Use Apache License 2.0

--- src/main/js/charts.js
+++ src/main/js/charts.js
@@ -1,9 +1,18 @@
-/*!
- * (c) 2012-2016 Paul Vorbach.
+/*
+ * Copyright 2012-2017 the original author or authors.
  *
- * MIT License (https://vorba.ch/license/mit.html)
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 var querystring = require('querystring');
 var escapeHtml = require('./escape-html.js');
 var moment = require('moment');
@@ -59,7 +68,7 @@ function showChart(id, title, data, xAxisType, xAxisTitle, cats) {
             }
         },
         subtitle: {
-            text: typeof document.ontouchstart == 'undefined' ?
+            text: typeof document.ontouchstart === 'undefined' ?
                 'Click and drag in the plot to zoom in' :
                 'Drag your finger over the plot to zoom in',
             style: {
