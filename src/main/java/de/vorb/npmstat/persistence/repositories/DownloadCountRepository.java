569f9dc13719f82eea2a98ab889679849fb3f334
Paul Vorbach <paul@vorba.ch>
2017-11-22 21:41:53 +0100

    Use TreeMap in DownloadCountRepository
    to enforce correct value order

--- src/main/java/de/vorb/npmstat/persistence/repositories/DownloadCountRepository.java
+++ src/main/java/de/vorb/npmstat/persistence/repositories/DownloadCountRepository.java
@@ -2,6 +2,7 @@ package de.vorb.npmstat.persistence.repositories;
 
 import de.vorb.npmstat.persistence.jooq.tables.records.DownloadCountRecord;
 
+import com.google.common.collect.Lists;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import static de.vorb.npmstat.persistence.jooq.Tables.DOWNLOAD_COUNT;
@@ -18,6 +20,8 @@ import static de.vorb.npmstat.persistence.jooq.Tables.PACKAGE;
 @Repository
 @RequiredArgsConstructor
 public class DownloadCountRepository {
+
+    private static final int INSERT_BATCH_SIZE = 128;
 
     private final DSLContext dslContext;
 
@@ -28,18 +32,26 @@ public class DownloadCountRepository {
                         .join(PACKAGE).on(DOWNLOAD_COUNT.PACKAGE_ID.eq(PACKAGE.ID)))
                 .where(PACKAGE.NAME.in(packageNames))
                 .and(DOWNLOAD_COUNT.DATE.between(from, until))
+                .orderBy(DOWNLOAD_COUNT.DATE.asc())
                 .fetch()
                 .stream()
                 .collect(Collectors.groupingBy(
                         record -> record.get(PACKAGE.NAME),
                         Collectors.toMap(
                                 record -> record.get(DOWNLOAD_COUNT.DATE),
-                                record -> record.get(DOWNLOAD_COUNT.COUNT))));
+                                record -> record.get(DOWNLOAD_COUNT.COUNT),
+                                (a, b) -> {
+                                    throw new RuntimeException(
+                                            String.format("Duplicate key for values %d and %d", a, b));
+                                },
+                                TreeMap::new
+                        )
+                ));
     }
 
     public void insert(List<DownloadCountRecord> records) {
-        dslContext.batchInsert(records)
-                .execute();
+        Lists.partition(records, INSERT_BATCH_SIZE)
+                .forEach(batch -> dslContext.batchInsert(records).execute());
     }
 
 }
