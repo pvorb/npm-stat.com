245f122e405833d60a05fd0d59221b6bec4c035a
Paul Vorbach <paul@vorba.ch>
2017-11-22 21:40:50 +0100

    Add constants about API

--- src/main/java/de/vorb/npmstat/clients/downloads/DownloadsClient.java
+++ src/main/java/de/vorb/npmstat/clients/downloads/DownloadsClient.java
@@ -1,14 +1,17 @@
 package de.vorb.npmstat.clients.downloads;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 @FeignClient(name = "downloads", url = "${npm.api.baseUrl}/downloads/")
 public interface DownloadsClient {
+
+    ZoneId API_TIME_ZONE =  ZoneId.of("America/Los_Angeles");
+    LocalDate MINIMAL_DATE = LocalDate.parse("2015-01-10");
 
     @GetMapping("range/{from}:{until}/{package}")
     DownloadsJson getPackageDownloadsForTimeRange(
