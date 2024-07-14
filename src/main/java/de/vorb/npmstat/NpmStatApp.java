687dc90fa3fa3a10241acfc7483c9dc5c6259667
Paul Vorbach <paul.vorbach@device-insight.com>
2024-07-14 02:38:55 +0200

    Build JAR instead of WAR

--- src/main/java/de/vorb/npmstat/NpmStatApp.java
+++ src/main/java/de/vorb/npmstat/NpmStatApp.java
@@ -18,8 +18,6 @@ package de.vorb.npmstat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.format.Formatter;
@@ -31,15 +29,10 @@ import java.util.Locale;
 
 @SpringBootApplication
 @EnableFeignClients
-public class NpmStatApp extends SpringBootServletInitializer {
+public class NpmStatApp {
 
     public static void main(String... args) {
         SpringApplication.run(NpmStatApp.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(NpmStatApp.class);
     }
 
     @Bean
